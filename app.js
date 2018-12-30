e2e821e931b013c736b4d66b93eec9bb50e8bfee
Sai Prasad <sai.prasad@rocketmail.com>
2018-12-30 20:42:00 +0530

    db config added

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const express = require('express');
-const express = require('express');
+const path = require('path');
+
+const db = require('./config/database');
+db.authenticate()
+    .then( () => console.log('DB connected'))
+    .catch(err => console.log('Error', err))
+
+
+const app = express();
+
+// body parser
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req, res) => res.send('index'))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , console.log(`server started on port : ${PORT}`))
