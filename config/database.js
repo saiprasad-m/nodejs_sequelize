61016127235f2fe78af98c0bbe68524cbb78b730
Sai Prasad <sai.prasad@rocketmail.com>
2018-12-30 21:27:00 +0530

    Added dialectOptions for database connection

--- config/database.js
+++ config/database.js
@@ -1,18 +1,16 @@
-var pg = require('pg');
-pg.defaults.ssl = true;
-
 const Sequelize = require('sequelize');
 module.exports = new Sequelize('d2a0i6uuev19di', 'apxlwbdjvbvdag', '8c17069b34b7f46493be895d296b325a62aa8200bffa0f8efa32227731073e52', {
     host: 'ec2-107-20-211-10.compute-1.amazonaws.com',
     dialect: 'postgres',
     operatorsAliases: false,
     ssl: true,
-
+    dialectOptions: {
+        ssl: true
+    }, 
     pool: {
         max: 5,
         min: 0,
         acquire: 90000,
         idle: 40000
     }
-
 })
